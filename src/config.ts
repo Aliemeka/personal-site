1e997c4f2daebcafaf3d3b86ac351e039c3d5425
Emeka Allison <emekaallison4@gmail.com>
2022-12-26 20:34:02 +0100

    Homepage for Emeka

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Emeka Allison";
+export const SITE_DESCRIPTION =
+  "I love building in public and collaborating with different people";
