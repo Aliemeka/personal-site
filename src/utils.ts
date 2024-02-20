f105b0f530ee020290f430d12c1c9e6acf2935e8
Emeka Allison <emekaallison4@gmail.com>
2024-02-20 01:50:32 +0100

    Fix: Time formatter

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 export const convertDate = (date: string) => {
-  const dateString = "2023-06-26T00:00:00.000Z";
-  const dateObj = new Date(dateString);
+  const dateObj = new Date(date);
   const formattedDate = dateObj.toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
@@ -8,3 +7,9 @@ export const convertDate = (date: string) => {
   });
   return formattedDate;
 };
+
+export function compareDates(a: string, b: string) {
+  const dateA: any = new Date(a);
+  const dateB: any = new Date(b);
+  return dateB - dateA;
+}
